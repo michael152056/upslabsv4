6146308a406bcb00aa3439cb15c29fbb04447091
Michael Robles <michaeljavier26@hotmail.com>
2022-01-09 22:54:47 -0500

    holaa

---
+++ src/app/actualizar-datos/actualizar-datos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-actualizar-datos',
+  templateUrl: './actualizar-datos.component.html',
+  styleUrls: ['./actualizar-datos.component.css']
+})
+export class ActualizarDatosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
